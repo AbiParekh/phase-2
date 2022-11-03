0928976a15c3f0b16ea1ab12a241d6be2a3af093
Abhishek Parekh <32465698+AbiParekh@users.noreply.github.com>
2022-11-02 21:48:28 -0400

    Create ReducerLibrary.h

---
+++ ReduceDLL/ReducerLibrary.h
@@ -0,0 +1,14 @@
+#pragma once
+//ReducerLibrary.h - contains declarations of math functions
+#include "Reduce.h"
+
+#pragma once
+
+#ifdef REDUCERLIBRARY_EXPORTS 
+#define REDUCERLIBRARY_API __declspec(dllexport)
+#else 
+#define REDUCERLIBRARY_API __declspec(dllimport)
+#endif
+
+
+//DLL header file for ReducerLibrary encapsulating reduce class
